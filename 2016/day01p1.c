3505b82bec2c553dc289cf144a2e3c1fd0cb550f
Will Eccles <will@eccles.dev>
2023-09-17 13:52:10 -0400

    2016d1p1: save 4 bytes

--- 2016/day01p1.c
+++ 2016/day01p1.c
@@ -1,1 +1,1 @@
-x,y,v;main(d,n){for(;scanf("%c%d, ",&d,&n)&&d>75;)*((v=v+~d&3)&1?&x:&y)+=v>1?-n:n;printf("%d",abs(x)+abs(y));}
+x,y,v;main(d,n){for(;scanf("%c%d, ",&d,&n)>1;)*((v=v+~d&3)&1?&x:&y)+=v>1?-n:n;printf("%d",abs(x)+abs(y));}
